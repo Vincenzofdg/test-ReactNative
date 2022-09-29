fe9daf8fdce5102410ca6d0c38c13539fed1ac12
Vicnenzofdg <vfdgiacomo@gmail.com>
2022-09-28 22:59:16 -0300

    feat: App.js inside src file

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tets up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
